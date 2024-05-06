dc6b4bf207e75c84643131f08e1776f3131d56bd
AlexGathecha <alex.gathecha@student.moringaschool.com>
2024-05-06 13:59:54 +0300

    done

---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './Nav.css';
+function Nav() {
+  return <div className='robots'> RoboApp</div>;
+}
+
+export default Nav;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import  theme  from "./theme";
 import { store } from "./redux";
 import ReactDOM from "react-dom/client";
